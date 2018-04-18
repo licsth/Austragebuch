ab8e7f4abe4248d39fd1720636db67235499edb2
Linda Thelen <linda.thelen@arcor.de>
2018-04-18 19:49:47 +0200

    Boolean format

--- processing/zurücktragenp.php
+++ processing/zurücktragenp.php
@@ -11,10 +11,10 @@ else {
     else{
         include 'dbh.php';
         $uid = $_SESSION['uid'];
-        $sql = "UPDATE eintrag SET isback=1 WHERE uid='$uid' AND isback IS NULL OR isback=0";
+        $sql = "UPDATE eintrag SET isback=1 WHERE uid='$uid' AND isback=0";
         $result = mysqli_query($conn, $sql);
         
-        $sql = "UPDATE schueler SET ausgetragen=NULL WHERE uid='$uid'";
+        $sql = "UPDATE schueler SET ausgetragen=0 WHERE uid='$uid'";
         $result = mysqli_query($conn, $sql);
         
         header("Location: ../schueler.php?src=zurücktragen");
