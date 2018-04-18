4f16433d8cabb22ee4616a4e6225597c2a3ae5b0
Linda Thelen <linda.thelen@arcor.de>
2018-04-18 23:26:20 +0200

    Dokumentation

--- besuch.php
+++ besuch.php
@@ -1,26 +1,27 @@
 <?php
-    session_start();
-
+session_start();
 include 'dbh.php';
+
+//Weiterleitung, falls Zugriffsberechtigungen fehlen
 if(!isset($_SESSION['uid'])){
     header("Location: index.php");
 }
 if($_SESSION['role'] != 'sozpaed'){
     header("Location: logout.php");
 }
+
 $uid = $_SESSION['uid'];
 
 ?>
 <html>
 <head>
     <link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Bootstrap-Theme -->
     <title>Besuchsankündigungen</title>
 </head>
 <body role="document">
     <nav class="navbar navbar-default">
       <div class="container-fluid">
-        <!-- Titel und Schalter werden für eine bessere mobile Ansicht zusammengefasst -->
+        <!-- Navigation -->
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
             <span class="sr-only">Navigation ein-/ausblenden</span>
@@ -31,13 +32,14 @@ $uid = $_SESSION['uid'];
           <a class="navbar-brand" href="#">Austragebuch</a>
         </div>
 
-        <!-- Alle Navigationslinks, Formulare und anderer Inhalt werden hier zusammengefasst und können dann ein- und ausgeblendet werden -->
+        <!-- Hauptmenü -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
             <li><a href="sozpaed.php">Start <span class="sr-only">(aktuell)</span></a></li>
             <li><a href="austragebuch.php?show=all">Austragebuch</a></li>
             <li class="active"><a href="#">Besuchsankündigungen</a></li>
           </ul>
+            <!-- rechtes Menü (persönliche Einstellungen) -->
           <ul class="nav navbar-nav navbar-right">
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><?php echo $uid; ?> <span class="caret"></span></a>
@@ -48,12 +50,15 @@ $uid = $_SESSION['uid'];
               </ul>
             </li>
           </ul>
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container-fluid -->
+        </div>
+      </div>
     </nav>
+    
+    <!-- Main -->
     <div class="container theme-showcase" role="main">
         <?php
         if(!empty($_GET['src'])){
+            // Alerts, z.B. nach Besuchsbestätigung
             if($_GET['src'] == "besuch"){
                 echo "<div class='alert alert-success alert-dismissable' role='alert'><button type='button' class='close' data-dismiss='alert' aria-label='Schließen'><span aria-hidden='true'>&times;</span></button>Besuch wurde erfolgreich bestätigt.</div>";
             }
@@ -61,7 +66,7 @@ $uid = $_SESSION['uid'];
         ?>
         <div class="page-header">
   <h1>Unbestätigte Besuchsankündigungen (<?php
-                
+                // Anzahl unbestätigter Besuchankündigungen
                 $sql = "SELECT COUNT(name) FROM gast WHERE bestaetigt=0";
                 $result = mysqli_query($conn, $sql);
                 $row = mysqli_fetch_assoc($result);
@@ -72,6 +77,7 @@ $uid = $_SESSION['uid'];
     
         <br>
         <?php
+        // Auswahl aller unbestätigten Besuche
         $sql = "SELECT * FROM gast WHERE bestaetigt=0";
         $result = mysqli_query($conn, $sql);
         while($row = mysqli_fetch_assoc($result)){
@@ -80,8 +86,9 @@ $uid = $_SESSION['uid'];
             $zeitraum = $row['zeitraum'];
             $uid = $row['schueler_uid'];
             $antrag = $row['antrag'];
+            //Datumsformatierung: Wochentag, dd. MMM YYY, hh:mm Uhr
             $date = DateTime::createFromFormat('Y-m-d H:i:s', $antrag);
-            $antrag = $date->format('D, d. M y, H:i') . " Uhr";
+            $antrag = $date->format('D, d. M Y, H:i') . " Uhr";
 
             $sql2 = "SELECT * FROM user WHERE uid='$uid'";
             $result2 = mysqli_query($conn, $sql2);
@@ -89,7 +96,8 @@ $uid = $_SESSION['uid'];
 
             $first = $row2['first'];
             $last = $row2['last'];
-
+            
+            // Für jeden Besuch ein Panel mit Name des Gastes, des Schülers, Zeitraum und Zeitpunkt des Antrags
             echo "<div class='panel panel-default'>
             <div class='panel-heading'>
     <h3 class='panel-title'>Besuch für $first $last</h3>
